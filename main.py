35b2839857e8cbdeff4a028f45464782bd085b97
Dmitriy Serbeniuk <dserbenyukgood@gmail.com>
2024-04-04 10:04:14 +0200

    add load_dotenv

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 import boto3
 import json
 from datetime import date
 import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def assume_role(role_arn, session_name):
     # Create a new STS client
@@ -25,82 +30,82 @@ def assume_role(role_arn, session_name):
 
     return session
 
-role_arn = 'arn:aws:iam::420779746987:role/RoleCostExplorerAPI'
-# Session name
-session_name = 'AssumedRoleSession'
+def get_billed_resources():
+    role_arn = 'arn:aws:iam::420779746987:role/RoleCostExplorerAPI'
+    # Session name
+    session_name = 'AssumedRoleSession'
 
-# Assume the IAM role
-session = assume_role(role_arn, session_name)
+    # Assume the IAM role
+    session = assume_role(role_arn, session_name)
 
-# Accessing Cost Explorer API
-client = session.client('ce')
+    # Accessing Cost Explorer API
+    client = session.client('ce')
 
-# StartDate = 1st date of current month, EndDate = Todays date
-start_date = str(date(year=date.today().year, month=date.today().month, day=1).strftime('%Y-%m-%d'))
-end_date = str(date.today())
+    # StartDate = 1st date of current month, EndDate = Todays date
+    start_date = str(date(year=date.today().year, month=date.today().month, day=1).strftime('%Y-%m-%d'))
+    end_date = str(date.today())
 
-print(f'StartDate:{start_date} - EndDate:{end_date}\n')
+    print(f'StartDate:{start_date} - EndDate:{end_date}\n')
 
-# The get_cost_and_usage operation is a part of the AWS Cost Explorer API, which allows you to programmatically retrieve cost and usage data for your AWS accounts.
-response = client.get_cost_and_usage(
-    TimePeriod={
-        'Start': start_date,
-        'End': end_date
-    },
-    Granularity='MONTHLY',
-    Metrics=['UnblendedCost'],
-    Filter={
-        "Not": {
-            'Dimensions': {
-                'Key': 'RECORD_TYPE',
-                'Values': ['Credit', 'Refund']
+    # The get_cost_and_usage operation is a part of the AWS Cost Explorer API, which allows you to programmatically retrieve cost and usage data for your AWS accounts.
+    response = client.get_cost_and_usage(
+        TimePeriod={
+            'Start': start_date,
+            'End': end_date
+        },
+        Granularity='MONTHLY',
+        Metrics=['UnblendedCost'],
+        Filter={
+            "Not": {
+                'Dimensions': {
+                    'Key': 'RECORD_TYPE',
+                    'Values': ['Credit', 'Refund']
+                }
             }
-        }
-    },
-    GroupBy=[
-        {
-            'Type': 'DIMENSION',
-            'Key': 'SERVICE'
-        }
-    ]
-)
+        },
+        GroupBy=[
+            {
+                'Type': 'DIMENSION',
+                'Key': 'SERVICE'
+            }
+        ]
+    )
 
-mydict = response
-resource_name = []
-resource_cost = []
+    mydict = response
+    resource_name = []
+    resource_cost = []
 
-total_resources_active = len(mydict['ResultsByTime'][0]['Groups'])
+    total_resources_active = len(mydict['ResultsByTime'][0]['Groups'])
 
-for i in range(total_resources_active):
-    a = (mydict['ResultsByTime'][0]['Groups'][i].values())
-    b = list(a)
-    resource_name.append(b[0][0])
-    resource_cost.append(float(b[1]['UnblendedCost']['Amount']))
+    for i in range(total_resources_active):
+        a = (mydict['ResultsByTime'][0]['Groups'][i].values())
+        b = list(a)
+        resource_name.append(b[0][0])
+        resource_cost.append(float(b[1]['UnblendedCost']['Amount']))
 
-dict0 = {}
+    dict0 = {}
 
-for i in range(total_resources_active):
-    dict0[resource_name[i]] = resource_cost[i]
+    for i in range(total_resources_active):
+        dict0[resource_name[i]] = resource_cost[i]
 
-# billed_resources = {k: v for k, v in dict0.items() if v}
+    # Filter out resources with a cost of $0.00
+    billed_resources = {k: v for k, v in dict0.items() if v > 0}
 
-# print(f'Current Billed Resources of this month:-', json.dumps(billed_resources, indent=4, sort_keys=True))
-# print(f'Active Resources:-', json.dumps(resource_name, indent=4, sort_keys=True))
-    
-# Filter out resources with a cost of $0.00
-billed_resources = {k: v for k, v in dict0.items() if v > 0}
+    return billed_resources, resource_name
 
-print(f'Current Billed Resources of this month:-', json.dumps(billed_resources, indent=4, sort_keys=True))
-print(f'Active Resources:-', json.dumps(resource_name, indent=4, sort_keys=True))
-
-
-TELEGRAM_API_TOKEN = "7115366129:AAF-6Df5ci-ZBrDi7D8X_R7AziXf1EWTvRE"
+TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 TELEGRAM_CHAT_ID = "364790462"
 
-def send_telegram_message(billed_resources, resource_names):
-    message = "Current Billed Resources of this month:\n\n"
+def send_telegram_message(billed_resources, active_resources):
+    billed_message = "Current Billed Resources of this month:\n\n"
     for resource, cost in billed_resources.items():
-        message += f"{resource}: ${cost:.2f}\n"
+        billed_message += f"{resource}: ${cost:.2f}\n"
+
+    active_message = "Active Resources:\n\n"
+    for resource in active_resources:
+        active_message += f"{resource}\n"
+
+    message = billed_message + "\n" + active_message
 
     url = f'https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage'
     params = {
@@ -115,4 +120,8 @@ def send_telegram_message(billed_resources, resource_names):
     else:
         print("Message sent successfully to Telegram.")
 
-send_telegram_message(billed_resources, resource_name)
+# Get billed resources and active resources
+billed_resources, active_resources = get_billed_resources()
+
+# Call send_telegram_message function to send the billed resources and active resources to Telegram
+send_telegram_message(billed_resources, active_resources)
